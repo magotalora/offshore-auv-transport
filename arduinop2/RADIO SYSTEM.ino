b87bb9ed2098e55b4607fc5eedc44ba5a4b4161a
Hubi2883 <96060382+Hubi2883@users.noreply.github.com>
2022-04-12 15:38:24 +0200

    Update and rename arduinop2.ino to RADIO SYSTEM.ino

--- arduinop2/RADIO SYSTEM.ino
+++ arduinop2/RADIO SYSTEM.ino
@@ -1,10 +1,29 @@
 #include "cc1000.h"
 #include <TinyGPS.h>
+#include <DHT.h>
 float lat, lon;
 int year;
 byte month, day, hour, minute, second, hundredths;
 TinyGPS gps;
 Cc1000 trx;
+
+const int leakdigital = 4; // leak pin
+
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+
+//////////////// SETUPS OF MODULES BELOW
+
+
+void leaksetup(){
+  pinMode(leakdigital, INPUT);
+}
+
+void dhtsetup(){
+    dht.begin();
+}
 
 // the setup function runs once when you press reset or power the board
 void txsetup() {
@@ -39,19 +58,71 @@ void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
   txsetup();
+  dhtsetup();
 }
+
+
+//////////////// FUNCTIONS OF SENSORS BELOW
+
+bool checkleak()
+{
+  if (digitalRead(capteur_D) == LOW) {
+    Serial.println("Digital value : wet"); 
+    return true;
+    delay (10)
+  } else {
+    Serial.println("Digital value : dry");
+    return false;
+  }
+}
+
+bool checkfire()
+{
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    //Print temp and humidity values to serial monitor
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    delay(1000); ///// double check
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+
+  if (digitalRead(temp) >= 70) {
+    Serial.println("Fire on board"); 
+    return true;
+    delay (10)
+  } else {
+    Serial.println("No fire on board");
+    return false;
+  }
+}
+
+
+
 // the loop function runs over and over again forever
 void txgpsproblem() {
+  checkleak();
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   String str = "";
   str = str + "$$$$$$$$ @@@@@@@@ ######## $$$$$$$$ "
         + "GPS not detected"
-        + " Modem mode: " + trx.get_modem_mode()
-        + " Power: " + trx.get_power()
-        + " Deviation: " + trx.get_deviation() + " Hz"
-        + " Bitrate: " + trx.get_bitrate() + " bps"
-        + " Frequency: " + trx.get_frequency() + " Hz"
+        + " Frequency: " + trx.get_frequency() + "Hz"
         + " $$$$$$$$\n";
+    if (checkleak()==true){
+    str = str + "Leak on board %#$$#$#$#$##$  "
+    }
+    if (checkfire()==true){
+    str = str + "Fire on board %#$$#$#$#$##$  "
+  }
   Serial.write((char*)str.c_str());
   trx.send_data(str);
   trx.set_trx_mode(TX_MODE);
@@ -61,10 +132,17 @@ void txgpsproblem() {
 }
 void TXgps()
 {
+  checkleak();
   gps.f_get_position(&lat, &lon);
   gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths);
-  String str = "constanin is piuking on adam's pillow ";
+  String str = " ";
   str = str + "$$$$$$$$ @@@@@@@@ ######## $$$$$$$$" + "Latitude: " + String(lat, 6) + "Longitude: " + String(lon, 6);
+  if (checkleak()==true){
+    str = str + "Leak on board %#$$#$#$#$##$"
+  }
+  if (checkfire()==true){
+    str = str + "Fire on board %#$$#$#$#$##$  "
+  }  
   Serial.write((char*)str.c_str());
   trx.send_data(str);
   trx.set_trx_mode(TX_MODE);
